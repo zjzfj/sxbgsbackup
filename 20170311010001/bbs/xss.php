7129c5f294934108771907c4593b26912bf50fc7
zjzfj <chuanchuan_beibei@163.com>
2017-03-11 01:00:58 +0800

    20170311010057

---
+++ 20170311010001/bbs/xss.php
@@ -0,0 +1,60 @@
+<?php
+
+function xss_clean($data) {
+
+    if (empty($data)) {
+        return $data;
+    }
+
+    if (is_array($data)) {
+        foreach ($data as $key => $value) {
+            $data[xss_clean($key)] = xss_clean($value);
+        }
+        return $data;
+    }
+
+    $data = str_replace(array('&amp;', '&lt;', '&gt;'), array('&amp;amp;', '&amp;lt;', '&amp;gt;'), $data);
+    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/', '$1;', $data);
+    $data = preg_replace('/(&#x*[0-9A-F]+);*/i', '$1;', $data);
+
+    if (function_exists("html_entity_decode")) {
+        $data = html_entity_decode($data);
+    } else {
+        $trans_tbl = get_html_translation_table(HTML_ENTITIES);
+        $trans_tbl = array_flip($trans_tbl);
+        $data = strtr($data, $trans_tbl);
+    }
+
+    $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#i', '$1>', $data);
+
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#i', '$1=$2nojavascript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#i', '$1=$2novbscript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#', '$1=$2nomozbinding...', $data);
+
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#i', '$1>', $data);
+
+    $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+    do {
+        // Remove really unwanted tags
+        $old_data = $data;
+        $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+    } while ($old_data !== $data);
+
+    return $data;
+}
+
+$_GET = xss_clean($_GET);
+$_POST = xss_clean($_POST);
+
+
+$ext_arr = array('PHP_SELF', '_ENV', '_GET', '_POST', '_FILES', '_SERVER', '_COOKIE', '_SESSION', '_REQUEST',
+    'HTTP_ENV_VARS', 'HTTP_GET_VARS', 'HTTP_POST_VARS', 'HTTP_POST_FILES', 'HTTP_SERVER_VARS',
+    'HTTP_COOKIE_VARS', 'HTTP_SESSION_VARS', 'GLOBALS');
+$ext_cnt = count($ext_arr);
+for ($i = 0; $i < $ext_cnt; $i++) {
+    if (isset($_GET[$ext_arr[$i]]))
+        unset($_GET[$ext_arr[$i]]);
+}
