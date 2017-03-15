213abaf0e78221299f453b1dcf1ffc0bd257704b
zjzfj <chuanchuan_beibei@163.com>
2017-03-16 01:01:47 +0800

    20170316010147

---
+++ 20170316010001/celive/include/monitor.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class Monitor {
+function monitor() {
+$activeid = $_SESSION['cel_activeid'];
+$status = '2';
+$sql = "UPDATE `activity` SET `status`='".$status."' WHERE `id`='".$activeid."'";
+$GLOBALS['db']->query($sql);
+}
+}
+?>
