a67864df2a31ca4c4ce85161339c360048fbe69d
zjzfj <chuanchuan_beibei@163.com>
2017-07-10 01:00:51 +0800

    20170710010049

---
+++ 20170710010001/template/admin2/cache/make_type.php
@@ -0,0 +1,19 @@
+<div id="tagscontent" class="right_box">
+
+<form name="typeform" method="post" action="<?php echo front::$uri;?>">
+<table border="0" cellspacing="0" cellpadding="0" name="table1" id="table1" width="100%">
+<tbody>
+<tr>
+	<td width="19%" align="right">分类</span></td>
+	<td width="1%">&nbsp;</td>
+                        <td width="70%"><?php
+	$archive=archive::getInstance();
+	echo form::select('typeid',get('typeid'),type::option());
+	?>
+	&nbsp;&nbsp;
+	<?php echo form::submit('更新');
+	?>
+    </td></tr></tbody>
+</table>
+</form>
+</div>
