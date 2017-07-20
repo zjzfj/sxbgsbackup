6cf128e4089f16a2cc41df45daf296fd8d97c092
zjzfj <chuanchuan_beibei@163.com>
2017-07-21 01:01:42 +0800

    20170721010127

---
+++ 20170721010001/celive/languages/chinese.php
@@ -0,0 +1,122 @@
+<?php
+/**
+* CmsEasy Live http://www.cmseasy.cn 				  			 
+* by CmsEasy Live Team 							  						
+**
+* Software Version: CmsEasy Live v 1.2.0 					  				        
+* Copyright 2009 by: CmsEasy, (http://www.cmseasy.cn) 	  
+* Support, News, Updates at: http://www.cmseasy.cn 			  			  
+**
+* This program is not free software; you can't may redistribute it and modify it under	  
+**
+* This file contains configuration settings that need to altered                  
+* in order for CE Live to work, and other settings that            
+**/
+
+$lang['charset'] = 'utf-8';
+
+$lang['live_text_online'] = '客服在线';
+$lang['live_text_offline'] = '客服离线';
+$lang['powered_by'] = 'Powered By';
+$lang['error'] = '错误';
+$lang['contact_us'] = '联系我们';
+$lang['your_name'] = '您姓名';
+$lang['your_email'] = '您的邮箱';
+$lang['email_sent'] = '邮件已经发送到您的地址.';
+$lang['login'] = '登陆';
+$lang['logout'] = '登出';
+$lang['sys_conf'] = '系统配置';
+$lang['sys_conf_exit'] = '退出系统配置';
+$lang['live_help'] = '帮助';
+$lang['trouble_tickets'] = '问题';
+$lang['canned_messages'] = '消息';
+$lang['transcripts'] = '成绩单';
+$lang['my_details'] = '我的详细';
+$lang['generate_code'] = '生成代码';
+$lang['time'] = '时间';
+$lang['nick'] = '昵称';
+$lang['faq'] = '帮助';
+$lang['if_customer_info'] = '访客信息收集';
+$lang['tracker_refresh'] = '数据更新间隔';
+$lang['login_failure'] = '错误的用户或者密码';
+$lang['submit'] = '确认';
+$lang['cancel'] = '取消';
+$lang['status_online'] = '[客服在线]';
+$lang['status_offline'] = '[客服离线]';
+$lang['status_on'] = '[ON]';
+$lang['status_off'] = '[OFF]';
+$lang['sounds'] = '声音';
+$lang['hostname'] = '数据库主机';
+$lang['more_info'] = '更多信息';
+$lang['accept'] = '同意';
+$lang['continue'] = '下一步';
+$lang['all_departments'] = '所有部门';
+$lang['createcode_mode'] = '生成代码模式';
+$lang['createcode_mode_images'] = '图片模式';
+$lang['createcode_mode_text'] = '文字模式';
+$lang['step1'] = '安装第一步';
+$lang['step2'] = '安装第二步';
+$lang['step3'] = '安装第三步';
+$lang['step4'] = '安装第四步';
+$lang['step5'] = '安装第五步';
+$lang['step6'] = '安装第六步';
+$lang['step7'] = '安装第七步';
+$lang['step8'] = '安装第八步';
+$lang['step9'] = '安装第九步';
+$lang['step10'] = '安装第十步';
+$lang['select_lang'] = '选择你的语言';
+$lang['install_upgrade'] = '你是安装还是升级';
+$lang['install'] = '安装';
+$lang['upgrade'] = '升级';
+$lang['config_chmod'] = '在你进行下一步操作之前,请确保config.php文档的属性是可写属性(777).';
+$lang['database'] = '数据库';
+$lang['db_username'] = '数据库用户名';
+$lang['db_password'] = '数据库密码';
+$lang['username'] = '用户名';
+$lang['password'] = '密码';
+$lang['prefix'] = '数据库表前缀';
+$lang['installation'] = '安装配置';
+$lang['monitor_traffic'] = '交互监控';
+$lang['monitor_yes'] = '交互和请求';
+$lang['monitor_no'] = '请求';
+$lang['company_name'] = '公司名称';
+$lang['template'] = '模板风格';
+$lang['skip'] = '跳过';
+$lang['url'] = '程序地址';
+$lang['config_more'] = '更多配置在config.php文件.';
+$lang['config_manual'] = '如果你已经手动编辑了正确信息,你就可以跳过此步.';
+$lang['install_db'] = '请确认数据库是否存在.';
+$lang['upgrade_db'] = 'Your current HCL database tables will be upgraded';
+$lang['setup_complete'] = '恭喜,安装成功!';
+$lang['now_login'] = '你可以马上登陆后台管理.';
+$lang['setup_warning_login'] = '提示: 为了安全起见,请删除安装目录install';
+$lang['config_tips'] = '配置config.php成功';
+$lang['setup_arrive'] = '如果你刚完成安装,你需要删除你的安装的目录install,然后你就可以登录';
+$lang['cel_welcome'] = '欢迎使用 CmsEasy Live';
+$lang['readme'] = '请确认阅读了使用必读文档.';
+$lang['config_problems'] = '如果你稍后更改或者无法更改,你可以跳过此步.';
+$lang['live_server'] = '企业在线客服';
+$lang['close'] = '关闭';
+$lang['you_with_now'] = '您现在正与';
+$lang['communication'] = '沟通';
+$lang['consultative_content'] = '咨询内容';
+$lang['welcome_to_us_please_click'] = '欢迎对我们提出宝贵建议，请点击';
+$lang['guest'] = '留言';
+$lang['tsp'] = '传输';
+$lang['welcome_into'] = '欢迎进入';
+$lang['com_live_chat'] = '企业在线客服平台!';
+$lang['you_name'] = '姓名';
+$lang['please_input_you_name'] = '请填写姓名!';
+$lang['email'] = '邮件';
+$lang['please_input_you_email'] = '请填写邮箱!';
+$lang['c_tel'] = '电话';
+$lang['please_input_you_tel'] = '请填写电话!';
+$lang['collect_tips'] = '为了让我们能够提供优质服务,需要您填写一些基本信息.';
+$lang['welcome_i'] = '欢迎您!来自';
+$lang['welcome_f'] = '的朋友';
+$lang['welcome_h'] = '请问有什么可以帮助您的吗?';
+$lang['welcome_i'] = '欢迎您!来自';
+$lang['copy'] = '企业在线客服平台';
+$lang['please_wait_for_the_customer_service_in_the_background'] = '请等待客服在后台接通！';
+$lang['companyinfos'] = '在线客服简介';
+?>
